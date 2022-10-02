504347dfebcf998979405c6e1a57c2358223c740
Carla Marques <marques.carl97@gmail.com>
2022-10-02 19:27:52 -0300

    revisão e atualização do código

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,65 +1,138 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import * as style from "./style";
-import triangulo from "../../img/triangulo-retangulo.png";  
-import { MdCalculate } from 'react-icons/md';
-import { GrPowerReset } from 'react-icons/gr';
-
+import triangulo from "../../img/triangulo-retangulo.png";
+import { MdCalculate } from "react-icons/md";
+import { GrPowerReset } from "react-icons/gr";
 
 export default function Home() {
+  //Variáveis declaradas, referentes aos lados de um triangulo, e seus respectivos valores iniciais.
   const [hipotenusa, setHipotenusa] = useState(0);
   const [catetoB, setCatetoB] = useState(0);
   const [catetoC, setCatetoC] = useState(0);
-  const [RespostaCB, setRespostaCB] = useState("b");
-  const [RespostaCC, setRespostaCC] = useState("c");
-  const [RespostaH, setRespostaH] = useState("a");
-  let [message, setMessage] = useState("O teorema de Pitágoras é uma expressão matemática que relaciona os lados de um triângulo retângulo, informe seus catetos para encontrar a hipotenusa. =D"); 
-  function calcular() {
-    if((catetoB != 0) && (catetoC != 0) && (hipotenusa == 0 || isNaN(hipotenusa))){
+  const [respostaCB, setRespostaCB] = useState("b");
+  const [respostaCC, setRespostaCC] = useState("c");
+  const [respostaH, setRespostaH] = useState("a");  
+  let [message, setMessage] = useState(
+    "O teorema de Pitágoras é uma expressão matemática que relaciona os lados de um triângulo retângulo, informe seus catetos para encontrar a hipotenusa. =D"
+  );
+  
+  //Função a qual calcula a hipotenusa e os cateos.
+  function Calcular() {
+    //Condições necessárias para setar a variável.
+    if (
+      catetoB != 0 &&
+      catetoC != 0 &&
+      (hipotenusa == 0 || isNaN(hipotenusa))
+    ) {
       setHipotenusa((catetoB ** 2 + catetoC ** 2) ** (1 / 2));
-      setMessage('Boaa, você encontrou a hipotenusa do triângulo retângulo! =D'); 
-    } else if((catetoB != 0) && (catetoC == 0 || isNaN(catetoC)) && (hipotenusa != 0)){
+      setMessage(
+        "Boaa, você encontrou a hipotenusa do triângulo retângulo! =D"
+      );
+    } else if (
+      catetoB != 0 &&
+      (catetoC == 0 || isNaN(catetoC)) &&
+      hipotenusa != 0
+    ) {
       setCatetoC((hipotenusa ** 2 - catetoB ** 2) ** (1 / 2));
-      setMessage('Você encontrou o valor correspondente ao Cateto C. Muito bom!');
-    } else if((catetoB == 0 || isNaN(catetoB)) && (catetoC != 0) && (hipotenusa != 0)){
+      setMessage(
+        "Você encontrou o valor correspondente ao cateto C. Muito bom!"
+      );
+    } else if (
+      (catetoB == 0 || isNaN(catetoB)) &&
+      catetoC != 0 &&
+      hipotenusa != 0
+    ) {
       setCatetoB((hipotenusa ** 2 - catetoC ** 2) ** (1 / 2));
-      setMessage('Você encontrou o valor correspondente ao cateto B. Muito bom!');
-    }else{
-      alert('Para encontrar o resultado de um dos lados do triangulo, escolha apenas 2 valores!');
+      setMessage(
+        "Você encontrou o valor correspondente ao cateto B. Muito bom!"
+      );
+    } else {
+      alert(
+        "Para encontrar o resultado de um dos lados do triangulo, escolha apenas 2 valores!"
+      );
     }
   }
-
-  function limpar() {
-    setMessage("O teorema de Pitágoras é uma expressão matemática que relaciona os lados de um triângulo retângulo, informe seus catetos para encontrar a hipotenusa. =D")
+  //Função "clean", retornar as variáveis para seus estados iniciais.
+  function Limpar() {
+    setMessage(
+      "O teorema de Pitágoras é uma expressão matemática que relaciona os lados de um triângulo retângulo, informe seus catetos para encontrar a hipotenusa. =D"
+    );
     setCatetoB(0);
     setCatetoC(0);
     setHipotenusa(0);
-  }
-
+  } 
+  
   return (
     <style.Container>
       <style.Title>Calculadora Pitágoras</style.Title>
       <style.Subtitle>{message}</style.Subtitle>
       <style.Form>
         <style.AcharHipotenusa>
-          <style.Hipotenusa>{hipotenusa == 0 || isNaN(hipotenusa) ? RespostaH : Number.isInteger(hipotenusa) ? hipotenusa : hipotenusa.toFixed(2)}</style.Hipotenusa>
-          <style.CatetoB>{catetoB == 0 || isNaN(catetoB) ? RespostaCB : Number.isInteger(catetoB) ? catetoB : catetoB.toFixed(2)}</style.CatetoB>
-          <style.CatetoC>{catetoC == 0 || isNaN(catetoC) ? RespostaCC : Number.isInteger(catetoC) ? catetoC : catetoC.toFixed(2)}</style.CatetoC>
+          {/*Condições referentes as variáveis na imagem do triangulo*/}
+          <style.Hipotenusa>
+            {hipotenusa == 0 || isNaN(hipotenusa)
+              ? respostaH
+              : Number.isInteger(hipotenusa)
+              ? hipotenusa
+              : hipotenusa.toFixed(2)}
+          </style.Hipotenusa>
+          <style.CatetoB>
+            {catetoB == 0 || isNaN(catetoB)
+              ? respostaCB
+              : Number.isInteger(catetoB)
+              ? catetoB
+              : catetoB.toFixed(2)}
+          </style.CatetoB>
+          <style.CatetoC>
+            {catetoC == 0 || isNaN(catetoC)
+              ? respostaCC
+              : Number.isInteger(catetoC)
+              ? catetoC
+              : catetoC.toFixed(2)}
+          </style.CatetoC>
           <img src={triangulo} />
         </style.AcharHipotenusa>
         <style.AcharHipotenusa>
-          <label>Cateto <b>b:</b></label>
-          <style.Input type="number" placeholder={" "} value={catetoB} onChange={(e) => setCatetoB(parseFloat(e.target.value))}/>
-          <label>Cateto <b>c:</b></label>
-          <style.Input type="number" placeholder={" "} value={catetoC} onChange={(e) => setCatetoC(parseFloat(e.target.value))}/>
-          <label>Hipotenusa <b>a:</b></label>
-          <style.Input type="number" placeholder={" "} value={hipotenusa} onChange={(e) => setHipotenusa(parseFloat(e.target.value))} />
+          <label>
+            Cateto <b>b:</b>
+          </label>
+          {/*Valor do campo input/atribuição do valor da variável*/}
+          <style.Input
+            type="number"
+            placeholder={" "}
+            value={catetoB}
+            onChange={(e) => setCatetoB(parseFloat(e.target.value))}
+          />
+          <label>
+            Cateto <b>c:</b>
+          </label>
+          <style.Input
+            type="number"
+            placeholder={" "}
+            value={catetoC}
+            onChange={(e) => setCatetoC(parseFloat(e.target.value))}
+          />
+          <label>
+            Hipotenusa <b>a:</b>
+          </label>
+          <style.Input
+            type="number"
+            placeholder={" "}
+            value={hipotenusa}
+            onChange={(e) => setHipotenusa(parseFloat(e.target.value))}
+          />
         </style.AcharHipotenusa>
       </style.Form>
       <style.Teclado>
-        <style.ButtonTeclado onClick={limpar}><GrPowerReset/><p>Limpar</p></style.ButtonTeclado> 
-        <style.ButtonTecladoCalcular onClick={calcular}><MdCalculate/><p>Calcular</p></style.ButtonTecladoCalcular>
+        <style.ButtonTeclado onClick={Limpar}>
+          <GrPowerReset />
+          <p>Limpar</p>
+        </style.ButtonTeclado>
+        <style.ButtonTecladoCalcular onClick={Calcular}>
+          <MdCalculate />
+          <p>Calcular</p>
+        </style.ButtonTecladoCalcular>
       </style.Teclado>
     </style.Container>
   );
---
+++ src/Pages/Home/style.ts
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  margin: 5rem auto;
+  max-width: 700px;
+  background: #f9f9f9;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.14);
+  @media (max-width: 500px) {
+    box-shadow: none;
+    margin: 2rem auto;
+  }
+  border-radius: 30px;
+  padding: 65px 0px;
+`;
+
+export const Title = styled.h1`
+  color: black;
+  text-align: center;
+`;
+
+export const Subtitle = styled.p`
+  padding: 1rem 4.5rem;
+  font-weight: 500;
+  color: #505050;
+  text-align: center;
+`;
+export const Form = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+export const AcharHipotenusa = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem;
+  align-content: center;
+`;
+export const Hipotenusa = styled.h2`
+  position: absolute;
+  margin-top: 7rem;
+  margin-left: 8.5rem;  
+  
+`;
+export const CatetoB = styled(Hipotenusa)`
+  margin-top: 8rem;
+  margin-left: 0rem;
+`;
+export const CatetoC = styled(Hipotenusa)`
+  margin-top: 16rem;
+  margin-left: 7rem;
+`;
+
+export const Input = styled.input`
+  border: 2px #dbdbdb solid;
+  background: white;
+  border-radius: 10px;
+  min-width: 13rem;
+  height: 2rem;
+  color: black;
+  padding: 10px;
+  margin: 0.5rem 0 1.5rem 0;
+  box-shadow: 0px 5.44492px 5.44492px rgba(0, 0, 0, 0.05);
+  :hover {
+    border-color: #24a952;
+  }
+  :focus {
+    outline: none;
+    border-color: #24a952;
+  }
+`;
+
+export const Teclado = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  max-width: 600px;
+`;
+
+export const ButtonTeclado = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  width: 13rem;
+  height: 81px;
+  background: #ffffff;
+  border: 1px solid #dbdbdb;
+  box-shadow: -4px 4px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+  color: black;
+  font-size: 1.4rem;
+  font-weight: 700;
+  margin: 10px;
+  cursor: pointer;
+  :hover {
+    transition: 0.2s;
+    background: #f0f0f0;
+  }
+  p {
+    margin-left: 8px;
+  }
+`;
+export const ButtonTecladoCalcular = styled(ButtonTeclado)`
+  background: #24a952;
+  color: white;
+  :hover {
+    transition: 0.2s;
+    background: #0b9c3d;
+  }
+`;
